f9c853687ddf86db04f1d14717f6bb06ce3bdd3d
Ryan <ravalles10@gmail.com>
2022-11-21 14:56:13 +0900

    Add sketch ability and reset button

--- script.js
+++ script.js
@@ -10,3 +10,27 @@ function createGrid(x){
 
 }
 createGrid(16);
+
+// make event listener that colors 'square'
+
+let squares = document.getElementsByClassName("square");
+for(let square of squares){
+square.addEventListener('mouseover', () => {
+  square.style.backgroundColor = "black";
+});
+}
+
+//make reset button
+document.getElementById('reset').onclick = function resetGrid(){
+  for(let square of squares){
+     square.style.backgroundColor = "white";
+    
+}};
+  
+
+
+//make option for 10x10 20x20 or 30x30 grid
+
+
+//make option for black or red or blue hover color
+
